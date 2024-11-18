06e199abcea696804df08a8a97fefe13df02bd19
Stephen <ehlert.stephenj#gmail.com>
2024-11-18 13:13:08 -0700

    end of monday

--- src/hello_freertos.c
+++ src/hello_freertos.c
@@ -3,7 +3,7 @@
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
-
+#pragma GCC optimize ("O0")
 #include <stdio.h>
 
 #include "FreeRTOS.h"
@@ -13,8 +13,10 @@
 #include "pico/multicore.h"
 #include "pico/cyw43_arch.h"
 
-int count = 0;
+
 bool on = false;
+#define OUT_PIN 1
+
 
 #define MAIN_TASK_PRIORITY      ( tskIDLE_PRIORITY + 1UL )
 #define BLINK_TASK_PRIORITY     ( tskIDLE_PRIORITY + 2UL )
@@ -22,33 +24,37 @@ bool on = false;
 #define BLINK_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
 
 void blink_task(__unused void *params) {
-    hard_assert(cyw43_arch_init() == PICO_OK);
-    while (true) {
-        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, on);
-        if (count++ % 11) on = !on;
-        vTaskDelay(500);
+
+
+    while (true) {        
+        gpio_put(OUT_PIN, on);
+
+        uint32_t k; 
+        for (int i = 0; i < 3000000; i++) { 
+            uint32_t j = 0;
+            j = ((~j >> i) + 1) * 27644437; 
+            k = j; 
+        } 
+        
+
+
+        on = !on;
+        //vTaskDelay(500);
     }
 }
 
-void main_task(__unused void *params) {
-    xTaskCreate(blink_task, "BlinkThread",
-                BLINK_TASK_STACK_SIZE, NULL, BLINK_TASK_PRIORITY, NULL);
-    char c;
-    while(c = getchar()) {
-        if (c <= 'z' && c >= 'a') putchar(c - 32);
-        else if (c >= 'A' && c <= 'Z') putchar(c + 32);
-        else putchar(c);
-    }
-}
+
 
 int main( void )
 {
     stdio_init_all();
+    gpio_init(OUT_PIN);
+    gpio_set_dir(OUT_PIN, GPIO_OUT);
     const char *rtos_name;
     rtos_name = "FreeRTOS";
     TaskHandle_t task;
-    xTaskCreate(main_task, "MainThread",
-                MAIN_TASK_STACK_SIZE, NULL, MAIN_TASK_PRIORITY, &task);
+    xTaskCreate(blink_task, "BlinkThread",
+                 BLINK_TASK_STACK_SIZE, NULL, BLINK_TASK_PRIORITY, NULL);
     vTaskStartScheduler();
     return 0;
 }
